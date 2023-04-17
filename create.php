83ac17bcdb8a2f5f88447db3d627a5ebc4271e12
Dima Kuznecov <kud3544@gmail.com>
2023-04-17 19:44:59 +0300

    git commit

--- create.php
+++ create.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="create.css">
+    <title>Create</title>
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php
 // Соединение с фйлом подключения к БД 
 require("indexbd.php");
